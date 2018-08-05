3dc176a2ad47e70dc35d0eee922778a3ee0faea5
nivedhamadhavan <40656001+nivedhamadhavan@users.noreply.github.com>
2018-08-05 17:53:09 +0530

    Add files via upload

---
+++ ex40.java
@@ -0,0 +1,25 @@
+	class Bank{  
+	int getRateOfInterest(){return 0;}  
+	}  
+	  
+	class SBI extends Bank{  
+	int getRateOfInterest(){return 8;}  
+	}  
+	  
+	class ICICI extends Bank{  
+	int getRateOfInterest(){return 7;}  
+	}  	class AXIS extends Bank{  
+	int getRateOfInterest(){return 9;}  
+	}  
+	int getRateOfInterest(){return 0;}  
+	  
+	class SBI extends Bank{  
+	int getRateOfInterest(){return 8;}  
+	}  
+	  
+	class ICICI extends Bank{  
+	int getRateOfInterest(){return 7;}  
+	}  
+	class AXIS extends Bank{  
+	int getRateOfInterest(){return 9;}  
+	}  
---
+++ ex45.java
@@ -0,0 +1,7 @@
+	class Animal{}  
+	class Dog1 extends Animal{  
+	  
+	 public static void main(String args[]){  
+	 Dog1 d=new Dog1();  
+	 System.out.println(d instanceof Animal);  
+	 }  	}  
